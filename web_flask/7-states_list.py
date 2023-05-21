f2ef683aec199bef8b626615e804e04063dc8403
Jenaide <jenaidesibolie@gmail.com>
2023-05-21 10:07:23 +0000

    bug fix in task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,20 +2,22 @@
 """
 Created by Jenaide Sibolie
 """
+from models import storage
+from models.state import State
 from flask import Flask, render_template
-from models import storage, State
 
 app = Flask(__name__)
 
+@app.teardown_appcontext
+def appcontext_teardown(self):
+    """uses Storage to fetch data from the storage engine"""
+    storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
-def States():
+def states():
     """Display a HTML page inside the tag BODY"""
     return render_template('7-states_list.html', states=storage.all(State))
-
-@app.teardown_appcontext
-def teardown_db(self):
-    storage.close()
 
 
 if __name__ == '__main__':
